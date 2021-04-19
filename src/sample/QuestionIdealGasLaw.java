274dd58d8ad28d7aff5da2833a331774850c18f2
DiegoOrtiz <2005193@newton.pe>
2021-04-18 19:55:58 -0500

    v5

--- src/sample/QuestionIdealGasLaw.java
+++ src/sample/QuestionIdealGasLaw.java
@@ -1,7 +1,7 @@
 package sample;
 
 import java.util.Random;
-
+import java.util.Scanner;
 
 public class QuestionIdealGasLaw {
 
@@ -13,21 +13,41 @@ public class QuestionIdealGasLaw {
         // Random number generating
 
         Random generator = new Random();
+        Scanner keyboard = new Scanner(System.in);
+        double sum = 0;
+        double clientAnswer ;
+        double answer;
 
-        int Pressure = generator.nextInt(120) + 30;
+
+        double Pressure = (generator.nextDouble()*120)+30;
+
+        int Pressure2 = generator.nextInt(120) + 30;
         int Moles = generator.nextInt(25);
         int Volume = generator.nextInt(80)+15;
 
         System.out.println("At a pressure of " + Pressure +  "atm, " + Moles + " Moles of an unknown compound are submerged in " + Volume + "L of water, what is the temperature of the room?");
+        clientAnswer = new Scanner(System.in).nextDouble();
 
-        double answer=(Pressure * Volume)/(Moles * 8.31);
+
+        answer=(Pressure * Volume)/(Moles * 8.31);
         double roundanswer= (double) Math.round(answer*100)/100;
         answer = roundanswer;
-        System.out.println(answer + "K");
-
-
+        if(clientAnswer == answer){
+            System.out.println("You are correct!");
+        }
+        else{
+            System.out.println(" Sorry, the correct answer is : " + answer + "K");
         }
 
 
     }
+}
+
+
+
+
+
+
+
+
 
