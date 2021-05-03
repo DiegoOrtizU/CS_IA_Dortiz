f24725b02ec87a89a5c13ecd3067aac4ed52e6c5
DiegoOrtiz <2005193@newton.pe>
2021-05-02 20:22:10 -0500

    v8

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,9 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Controller {
 @FXML  private AnchorPane MAINPANEL;
 @FXML  private AnchorPane IGLQUESTIONSPANEL;
@@ -64,8 +67,29 @@ public class Controller {
 
 
     public void IGLNEWQUESTION(){
-        IGLQUESTION.setText("Hello");
+        Random generator = new Random();
+        Scanner keyboard = new Scanner(System.in);
+        double answer;
+
+
+        double Pressure = (generator.nextDouble() * 120) + 30;
+        double Moles = (generator.nextDouble() * 25);
+        double Volume = (generator.nextDouble() * 80) + 15;
+
+        double roundPressure = (double) Math.round(Pressure * 100) / 100;
+        Pressure = roundPressure;
+
+        double roundMoles = (double) Math.round(Moles * 100) / 100;
+        Moles = roundMoles;
+
+        double roundVolume = (double) Math.round(Volume * 100) / 100;
+        Volume = roundVolume;
+
+        IGLQUESTION.setText("At a pressure of " + Pressure + "atm, " + Moles + " Moles of an unknown compound are submerged in " + Volume + "L of water, what is the temperature of the room?");
+
     }
+
+
 
     public void IGLREGISTERANSWERBTN(ActionEvent actionEvent) {
     }
